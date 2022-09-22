95a03e01d760aa29eee18746f79e6cb0c72bf1d0
apurvisnotcool <apurvpoddar44@gmail.com>
2022-09-22 17:46:27 +0530

    chapter 1 Code question 1

---
+++ Ch_1C1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "*****************************************\n";
+    cout << "       c c c         s s s s       !!\n";
+    cout << "     c       c     s         s     !!\n";
+    cout << "    c             s                !!\n";
+    cout << "   c               s               !!\n";
+    cout << "   c                s s s s        !!\n";
+    cout << "   c                         s     !!\n";
+    cout << "    c                         s    !!\n";
+    cout << "     c       c     s         s     !!\n";
+    cout << "       c c c         s s s s       00\n";
+    cout << "*****************************************\n";
+    cout << "Computer Science is Cool Stuff !!!\n";
+    
+    
+}
