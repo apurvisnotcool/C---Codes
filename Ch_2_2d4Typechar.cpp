cd9edc316984be5175e573adcad8e639010d0f93
apurvisnotcool <apurvpoddar44@gmail.com>
2022-09-27 20:57:34 +0530

    Chapter 2 Code 2

---
+++ Ch_2_2d4Typechar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char symbol1,symbol2,symbol3;
+    cout << "Enter two Initial, Without any Periods: \n";
+    cin >> symbol1>>symbol2;
+    cout << "The two Initials are : \n";
+    cout << symbol1 <<symbol2 << endl;
+    cout << "Once more a space :\n";
+    symbol3 = ' ';
+    cout << symbol1 << symbol3 << symbol2 << endl;
+    cout << "That's All.";
+
+    return 0;
+         
+
+}
